18424b8f5206b19b84a3edb6dec3397620f66f4b
Bl3f <christophe.blefari@gmail.com>
2018-05-05 12:56:44 +0200

    Add first test

--- gallery/tests.py
+++ gallery/tests.py
@@ -4,4 +4,12 @@ from django.urls import reverse
 
 class TestGalleryViews(TestCase):
     def test_upload_view(self):
-        pass
+        with open('gallery/assets/test_image.jpeg', 'rb') as image:
+            url = reverse('upload')
+
+            data = {
+                'image': image,
+                'name': 'Mon chat',
+            }
+
+            self.client.post(url, data=data)
--- test_image_resize/urls.py
+++ test_image_resize/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from gallery.views import upload
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upload/', upload, name='upload'),
 ]
