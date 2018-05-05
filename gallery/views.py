578d31bf831a29f549536d514bf5acaaa73609cc
Bl3f <christophe.blefari@gmail.com>
2018-05-05 13:25:26 +0200

    Clean

--- gallery/views.py
+++ gallery/views.py
@@ -5,7 +5,8 @@ from gallery.forms import ImageForm
 
 def upload(request):
     form = ImageForm(request.POST, request.FILES)
+
     if form.is_valid():
         form.save()
-        print(form)
-    return HttpResponse('')
+
+g    return HttpResponse('')
