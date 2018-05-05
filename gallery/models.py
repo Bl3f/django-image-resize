fc7f7ebd75e217afad53eed2ba2bd22fffea4fe4
Bl3f <christophe.blefari@gmail.com>
2018-05-05 14:33:35 +0200

    Factorize image path(s)

--- gallery/models.py
+++ gallery/models.py
@@ -3,16 +3,20 @@ from django.db import models
 from gallery.utils import minify_name
 
 
-def image_path(instance, filename):
-    return '{0}/{1}.jpeg'.format(instance.user, minify_name(instance.name))
+def image_path(instance, filename, size):
+    return '{0}/{1}{2}.jpeg'.format(instance.user, minify_name(instance.name), size)
+
+
+def image_normal_path(instance, filename):
+    return image_path(instance, filename, '')
 
 
 def image_medium_path(instance, filename):
-    return '{0}/{1}_medium.jpeg'.format(instance.user, minify_name(instance.name))
+    return image_path(instance, filename, '_medium')
 
 
 def image_small_path(instance, filename):
-    return '{0}/{1}_small.jpeg'.format(instance.user, minify_name(instance.name))
+    return image_path(instance, filename, '_small')
 
 
 class Image(models.Model):
@@ -24,6 +28,6 @@ class Image(models.Model):
 
 
 class UploadedImage(Image):
-    file = models.ImageField(upload_to=image_path)
+    file = models.ImageField(upload_to=image_normal_path)
     file_medium = models.ImageField(upload_to=image_medium_path)
     file_small = models.ImageField(upload_to=image_small_path)
